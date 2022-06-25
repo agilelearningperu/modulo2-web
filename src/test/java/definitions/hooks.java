c70c1f209d1aee6f84872d4dc0e365317d4e4502
agilelearningperu <aavilav5@gmail.com>
2022-06-25 01:52:24 -0500

    hooks

--- src/test/java/definitions/hooks.java
+++ src/test/java/definitions/hooks.java
@@ -22,7 +22,7 @@ public class hooks {
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--headless");
 			options.addArguments("--no-sandbox");
-            driver = new RemoteWebDriver(new URL("http://localhost:9222/"),options);
+            driver = new RemoteWebDriver(new URL("http://0.0.0.0:9222"),options);
 //            driver = new ChromeDriver();
             driver.manage().window().maximize();
         }else if (navegador.equals("firefox")){
