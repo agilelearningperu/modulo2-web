2d842a80214a7a0a51bdbcbbebf9faf5c2171848
agilelearningperu <aavilav5@gmail.com>
2022-06-25 01:10:42 -0500

    hooks remote

--- src/test/java/definitions/hooks.java
+++ src/test/java/definitions/hooks.java
@@ -6,18 +6,22 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class hooks {
     private static String navegador="chrome";
     public static WebDriver driver;
 
     @Before
-    public static void setUp(){
+    public static void setUp() throws MalformedURLException {
         if (navegador.equals("chrome")){
             System.setProperty("webdriver.chrome.driver","src/test/resources/drivers/chromedriver.exe");
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--headless");
-            driver = new ChromeDriver(options);
+            driver = new RemoteWebDriver(new URL("http://localhost:3000/webdriver"),options);
 //            driver = new ChromeDriver();
             driver.manage().window().maximize();
         }else if (navegador.equals("firefox")){
